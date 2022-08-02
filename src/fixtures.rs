ef356c47c11adfa4e703d871575e9492259d9835
Alex Kotov <kotovalexarian@gmail.com>
2022-08-03 01:31:03 +0400

    Rename local vars

--- src/fixtures.rs
+++ src/fixtures.rs
@@ -3,10 +3,10 @@ use super::*;
 use serde_json::{json, Value};
 
 pub fn meta_or_attrs() -> MetaOrAttrs {
-    let mut expected_meta_or_attrs: MetaOrAttrs = MetaOrAttrs::new();
-    expected_meta_or_attrs.insert("foo".into(), Value::Number(123.into()));
-    expected_meta_or_attrs.insert("bar".into(), Value::String("qwe".into()));
-    expected_meta_or_attrs
+    let mut meta_or_attrs: MetaOrAttrs = MetaOrAttrs::new();
+    meta_or_attrs.insert("foo".into(), Value::Number(123.into()));
+    meta_or_attrs.insert("bar".into(), Value::String("qwe".into()));
+    meta_or_attrs
 }
 
 pub fn meta_or_attrs_value() -> Value {
